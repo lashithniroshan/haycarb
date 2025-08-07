cf89dc8d73b065783a5047a89f0b61dca42c8cb9
Misfar Siddeek <asmmisfar@gmail.com>
2025-08-08 03:30:31 +0530

    overlay added for the chart page

--- app/(root)/tailor-made-for-you/ChartGenerator.tsx
+++ app/(root)/tailor-made-for-you/ChartGenerator.tsx
@@ -13,6 +13,64 @@ const ChartGenerator = () => {
 
   return (
     <section className="relative bg-white text-[#606060] pt-16 pb-20 px-4 overflow-hidden">
+      <div
+        aria-hidden="true"
+        className="absolute top-0 -right-20 lg:w-150 lg:h-150 bg-no-repeat bg-contain"
+        style={{
+          backgroundImage: "url(/images/water-spread.svg)",
+        }}
+      ></div>
+      <div
+        aria-hidden="true"
+        className="absolute
+                hidden lg:block
+                top-170 right-90
+                lg:top-78 lg:-right-35
+                xl:top-40 xl:-right-35
+                2xl:top-40 2xl:-right-35
+                md:w-100 md:h-100
+                lg:w-50 lg:h-50  
+                xl:w-90 xl:h-90
+                2xl:w-100 2xl:h-100
+                -rotate-15 scale-x-[-1] 
+                bg-no-repeat bg-contain opacity-25 z-40"
+        style={{
+          backgroundImage: "url(/images/innerpage/blue-whale.png)",
+        }}
+      ></div>
+
+      <div
+        aria-hidden="true"
+        className="absolute
+                  hidden 
+                  lg:block 
+                  left-15 
+                  lg:top-4/5 
+                  xl:top-6/7 
+                  w-56 h-56 
+                  md:w-72 md:h-72
+                  lg:w-50 lg:h-50  
+                  xl:w-72 xl:h-72
+                  transform scale-x-[-1] -rotate-20
+                  -translate-x-1/2 -translate-y-9/10 
+                  bg-no-repeat bg-contain opacity-25"
+        style={{
+          backgroundImage: "url(/images/innerpage/blue-whale.png)",
+        }}
+      ></div>
+
+      <div
+        aria-hidden="true"
+        className="absolute 
+                  top-300 -left-40
+                  lg:w-200 lg:h-200 
+                  scale-x-[-1]
+                  bg-no-repeat bg-contain"
+        style={{
+          backgroundImage: "url(/images/water-spread.svg)",
+        }}
+      ></div>
+
       {/* Description section */}
       <div className="lg:px-53 text-center">
         <h1 className="text-[#005881] text-[40px] font-normal">
