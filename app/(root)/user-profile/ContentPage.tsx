c4f6772d859aa31aeb442dc5839d6b0d1d556eae
Misfar Siddeek <asmmisfar@gmail.com>
2025-08-10 18:31:55 +0530

    matrics added.

--- app/(root)/user-profile/ContentPage.tsx
+++ app/(root)/user-profile/ContentPage.tsx
@@ -12,7 +12,7 @@ type MatricsType = { worth: string; title: string };
 
 type Content = {
   short_description: string;
-  matrics?: MatricsType[];
+  matrics: MatricsType[];
   chairman_and_director?: string;
   stratergy_and_performance?: React.ReactNode;
 };
@@ -28,35 +28,100 @@ const stakeholdersData: Record<StakeholderKey, Stakeholder> = {
     title: "Shareholders",
     icon: "/icons/user-profile/shareholder.gif",
     content: {
-      short_description: "Detailed content for Shareholders goes here...",
+      short_description:
+        "As the guiding current beneath our journey, you steer Haycarb’s course. Dive into a clear view of our performance, strategy, and sustainable growth.",
+      matrics: [
+        {
+          worth: "Rs. 12.10",
+          title: "Earnings Per Share",
+        },
+        { worth: "13.6%", title: "ROE" },
+        { worth: "Rs. 89.30", title: "Net Asset per Share" },
+        { worth: "4.6%", title: "Dividends Yield" },
+        { worth: "14.9%", title: "ROCE" },
+      ],
     },
   },
   employees: {
     title: "Employees",
     icon: "/icons/employees.svg",
     content: {
-      short_description: "Detailed content for Employees goes here...",
+      short_description:
+        "You are the power behind the wave. See how your commitment fuels our mission as we navigate new depths of innovation and impact together.",
+      matrics: [
+        {
+          worth: "2,026",
+          title: "No. of Employees",
+        },
+        { worth: "91%", title: "Retention rate" },
+        { worth: "17.3 h / Emp", title: "Average Training hours" },
+        { worth: "Rs. 4.9 Bn", title: "Payments to Employees" },
+        { worth: "Rs. 70 Mn", title: "Investment in Health and Safety" },
+      ],
     },
   },
   customers: {
     title: "Customers",
     icon: "/icons/customers.svg",
     content: {
-      short_description: "Detailed content for Customers goes here...",
+      short_description:
+        "With you, we will go farther and deeper. Discover how Haycarb delivers consistency, quality, and sustainability, like the steady movement of a whale through changing seas.",
+      matrics: [
+        {
+          worth: "90%",
+          title: "Customer Satisfaction Score",
+        },
+        { worth: "26", title: "New Customers" },
+        { worth: "16", title: "New Products Launched" },
+        { worth: "4", title: "Products in Pipeline" },
+        { worth: "Rs. 209.3 Mn", title: "Investment in R&D" },
+      ],
     },
   },
   suppliers: {
     title: "Suppliers",
     icon: "/icons/suppliers.svg",
     content: {
-      short_description: "Detailed content for Suppliers goes here...",
+      short_description:
+        "Strong partnerships keep our ecosystem thriving. Join us in building resilient, responsible supply chains that move as one - like pods working in harmony.",
+      matrics: [
+        {
+          worth: "Rs. 22.2 Bn",
+          title: "Payments to Suppliers",
+        },
+        { worth: "Rs. 20.6 Mn", title: "Investment in Capacity Building" },
+        {
+          worth: "500",
+          title: "Total no. of coconut shell and charcoal suppliers supported",
+        },
+        {
+          worth: "200",
+          title: "Total no. of other material and service Suppliers",
+        },
+        {
+          worth: "91%",
+          title: "% of spending on coconut shell and charcoal suppliers",
+        },
+      ],
     },
   },
   generalUser: {
     title: "General User",
     icon: "/icons/general-user.svg",
     content: {
-      short_description: "Detailed content for General Users goes here...",
+      short_description:
+        "As the guiding current beneath our journey, you steer Haycarb’s course. Dive into a clear view of our performance, strategy, and sustainable growth.",
+      matrics: [
+        {
+          worth: "Rs. 43.2Bn",
+          title: "Revenue",
+        },
+        { worth: "13.6%", title: "ROE" },
+        { worth: "2,026", title: "No. of Employees" },
+        { worth: "16", title: "New Products Launched" },
+        { worth: "Rs. 209.3 Mn", title: "Investment in R&D" },
+        { worth: "50.4", title: "Investment in CSR (Rs. Mn)" },
+      ],
     },
   },
 };
@@ -138,8 +203,31 @@ const ContentPage = () => {
         </div>
 
         {/* Tab Content */}
-        <div className="mt-8 p-4 bg-white rounded-lg shadow">
+        <p className="mt-10 py-4 lg:px-20 para text-center text-lg font-semibold text-[#005881]">
           {stakeholdersData[activeKey].content.short_description}
+        </p>
+
+        <div className="flex flex-wrap justify-center items-center gap-y-8 mt-18 text-center">
+          {stakeholdersData[activeKey].content?.matrics?.map((item, idx) => {
+            const totalItems =
+              stakeholdersData[activeKey].content?.matrics?.length || 0;
+            const isLast = idx === totalItems - 1;
+
+            return (
+              <div
+                key={idx}
+                className={`
+                            text-[#005881] px-8
+                            ${!isLast ? "border-r border-[#1D80A0]" : ""}
+                            basis-1/2 sm:basis-1/3 lg:basis-auto
+                            flex-shrink-0
+                        `}
+              >
+                <p className="text-[40px] font-bold">{item.worth}</p>
+                <p className="text-base">{item.title}</p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
