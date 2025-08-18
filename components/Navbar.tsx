dc3792b18deea54a67b9766823ac76b3622eaac9
lashith niroshan <niroshanmail@gmail.com>
2025-08-19 00:56:33 +0530

    logo size

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -202,7 +202,7 @@ const Navbar = ({
               height={isScrolled ? 72 : 100}
                className={`transition-all duration-600 ${
           isScrolled
-            ? 'w-[120px] h-[72px] max-[991px]:w-[90px] max-[991px]:h-[48px]' // Scrolled sizes
+            ? 'w-[100px] h-[52px] max-[991px]:w-[90px] max-[991px]:h-[48px]' // Scrolled sizes
             : 'w-[200px] h-[100px] max-[991px]:w-[120px] max-[991px]:h-[60px]' // Not scrolled sizes
         }`}
             />
